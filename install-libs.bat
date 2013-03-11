2d9fdbede7a6265b13748a12b751ea8295d7920e
alistairrutherford <alistair.rutherford@gmail.com>
2013-03-11 23:43:47 +0000

    Added missing libs to make the setup easier.

--- install-libs.bat
+++ install-libs.bat
@@ -6,4 +6,6 @@ call %MAVEN_HOME%\bin\mvn install:install-file -Dfile=libs\tween-engine-api.jar 
 
 call %MAVEN_HOME%\bin\mvn install:install-file -Dfile=libs\tween-engine-api-sources.jar -DgroupId=aurelienribon.tweenengine -DartifactId=tween-engine-api-sources -Dversion=6.3.3 -Dpackaging=jar -DgeneratePom=true
 
+call %MAVEN_HOME%\bin\mvn install:install-file -Dfile=libs\libgdx-fixtureatlas.jar -DgroupId=aurelienribon.fixtureatlas -DartifactId=libgdx-fixtureatlas -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+
 endlocal
